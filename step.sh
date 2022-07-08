4e11d6159092768bd3ac397270c4b46b13236d8d
Joshi <joshuafrancis.roman@rakuten.com>
2022-07-08 18:51:41 +0900

    Initial commit

---
+++ step.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -ex
+
+echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+
+#
+# --- Export Environment Variables for other Steps:
+# You can export Environment Variables for other Steps with
+#  envman, which is automatically installed by `bitrise setup`.
+# A very simple example:
+envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
+# Envman can handle piped inputs, which is useful if the text you want to
+# share is complex and you don't want to deal with proper bash escaping:
+#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
+# You can find more usage examples on envman's GitHub page
+#  at: https://github.com/bitrise-io/envman
+
+#
+# --- Exit codes:
+# The exit code of your Step is very important. If you return
+#  with a 0 exit code `bitrise` will register your Step as "successful".
+# Any non zero exit code will be registered as "failed" by `bitrise`.
+
+
+#GET PR ID
+latestCommitMessage=`git log -1 --pretty`
+prIdInitialRegex="Pull\srequest\s#[0-9]*:"
+
+prIdInitial=`echo $latestCommitMessage | grep $prIdInitialRegex -o`
+prId=`echo $prIdInitial | tr -dc [0-9]`
+
+
+if [ -z "$prId"]
+then
+   echo "Could not parse PR ID; most likely commit is not PR merge commit"
+
+else
+
+  #GET JIRA TICKETS
+
+  ticketRegex="${jira_project_key}-[0-9]*"
+
+  url="https://$bitbucket_baseurl/rest/api/1.0/projects/$bitbucket_project/repos/$bitbucket_repo/pull-requests/$prId"
+
+  #https://developer.atlassian.com/cloud/bitbucket/rest/api-group-pullrequests/#api-repositories-workspace-repo-slug-pullrequests-pull-request-id-get
+  prDetails=`curl --request GET \
+    --url "${url}" \
+    --header "Authorization: Bearer ${git_access_token}" \
+    --header 'Accept: application/json'`
+
+  prTitle=`echo $prDetails | jq -r .title`
+  prSourceBranch=`echo $prDetails | jq -r .fromRef.displayId`
+
+  #/diff and /commits have different behavior, but /commits are exactly what we see on bitbucket PR commits
+  prDetailsDiff=`curl --request GET \
+    --url "${url}/commits" \
+    --header "Authorization: Bearer ${token}" \
+    --header 'Accept: application/json'`
+
+  includedCommitMessages=`echo $prDetailsDiff | jq -r .values[].message`
+
+
+  jiraTicketsFromTitle=`echo $prTitle | grep $ticketRegex -o`
+  jiraTicketsFromBranchName=`echo $prSourceBranch | grep $ticketRegex -o`
+  jiraTicketsFromCommits=`echo $includedCommitMessages | grep $ticketRegex -o`
+
+  echo $jiraTicketsFromTitle
+  echo $jiraTicketsFromBranchName
+  echo $jiraTicketsFromCommits
+  
+  envman add --key JIRA_TICKETS_FROM_TITLE --value $jiraTicketsFromTitle
+  envman add --key JIRA_TICKETS_FROM_BRANCH_NAME --value jiraTicketsFromBranchName
+  envman add --key JIRA_TICKETS_FROM_COMMITS --value jiraTicketsFromCommits
+fi
